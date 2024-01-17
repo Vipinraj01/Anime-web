bee05d872d1c9145b306bd9f70fea5628497a5ad
Vipin Raj <114496486+Vipinraj01@users.noreply.github.com>
2024-01-17 12:34:12 +0530

    Add files via upload

---
+++ anime/new.py
@@ -0,0 +1,6 @@
+a=int(input("enter the a value :"))
+b=int(input("enter the b value :"))
+if a>b:
+    print("a is greater")
+else:
+    print("b is greater")
---
+++ anime/sum.py
@@ -0,0 +1,5 @@
+n=[1,1,2,3,4,6]
+count=0
+for i in n:
+    count=count+i
+print(count)
